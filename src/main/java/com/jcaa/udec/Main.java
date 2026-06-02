4a779b2a4baa89fcc2329193ba8721ee01b2222c
John Carlos Arrieta Arrieta <johncarlosarrietaarrieta@gmail.com>
2026-06-01 21:44:53 -0500

    feat: Mensaje usando log con la popular libreira SLF4J

--- src/main/java/com/jcaa/udec/Main.java
+++ src/main/java/com/jcaa/udec/Main.java
@@ -1,7 +1,12 @@
 package com.jcaa.udec;
 
-public class Main {
-    public static void main(String[] args) {
+import lombok.extern.slf4j.Slf4j;
 
-    }
+@Slf4j
+public class Main {
+  private static final String MENSAJE_INICIO = "Capa de Dominio normalizada con DDD y SOLID";
+
+  public static void main(String[] args) {
+    log.info(MENSAJE_INICIO);
+  }
 }
